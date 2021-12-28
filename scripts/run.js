3bed98e98a39a23946e2350bbebc4dc5fbe13de3
Nissan Dookeran <nissan.dookeran@gmail.com>
2021-12-29 09:17:01 +1000

    adds waves functions

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,21 @@
 
 (async () => {
     try {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+    
     console.log("Contract deployed to: ", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
     process.exit(0);
     }
     catch(error) {
