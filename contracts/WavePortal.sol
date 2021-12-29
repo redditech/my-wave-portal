511017b642988c094319a68cb5a8c854f3e56a09
Nissan Dookeran <nissan.dookeran@gmail.com>
2021-12-30 08:27:31 +1000

    adds message to contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,13 +8,47 @@ contract WavePortal {
 
     uint256 totalWaves;
 
+    /*
+    * Link to events in solidity: https://blog.chain.link/events-and-logging-in-solidity/
+    */
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+    * A struct is a custom datatype where we customise what we want to hold inside
+    * Link to structs https://www.tutorialspoint.com/solidity/solidity_structs.htm
+    */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    /*
+    * Add variable to store array of structs
+    * This is what will hold all the waves anyone sends me
+    */
+    Wave[] waves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
-    function wave() public {
+    // Require the function to have a message, the message the user sends us
+    function wave(string memory _message) public {
         totalWaves +=1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s has waved with message %s", msg.sender,  _message);
+
+        // Store the wave data in the array
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        //Emit an event
+        // Link to emit: https://medium.com/coinmonks/the-curious-case-of-emit-in-solidity-2d88913e3d9a
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    // Function to get all waves
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -11,19 +11,18 @@
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+    // Send a few waves
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-    waveCount = await waveContract.getTotalWaves();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-
-
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+    
     process.exit(0);
     }
     catch(error) {
